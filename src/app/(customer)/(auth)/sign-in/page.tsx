99a74221bc1ee73502c3ad6cb799cac979e1da80
sulthan rafi hendriansyah <sulthan19@gmail.com>
2025-01-06 18:11:53 +0700

    feat(auth): integrate customer sign-in functionality

--- src/app/(customer)/(auth)/sign-in/page.tsx
+++ src/app/(customer)/(auth)/sign-in/page.tsx
@@ -1,39 +1,66 @@
+"use client"
+
+import { ActionResult } from "@/types";
 import { Spectral } from "next/font/google";
+import { useFormState, useFormStatus } from "react-dom";
+import { SignIn } from "../lib/actions";
 
 const spectral = Spectral({
     weight: ['400', '700'],
     subsets: ['latin']
 });
 
+const initialFormState: ActionResult = {
+    error: ''
+}
+
+function SubmitButton() {
+    const { pending } = useFormStatus()
+
+    return (
+        <button disabled={pending} type="submit" className="p-[12px_24px] bg-[#0D5CD7] rounded-full text-center font-semibold text-white">{pending ? 'Loading...' : 'Sign In to My Account'}</button>
+    )
+}
+
 export default function SignInPage() {
+    const [state, formAction] = useFormState(SignIn, initialFormState)
+
     return (
         <div id="signin" className="bg-[#EFF3FA] min-h-screen pt-[30px] pb-[50px] flex flex-col">
             <div className="container max-w-[1130px] mx-auto flex flex-1 items-center justify-center py-5">
-                <form action="index.html" className="w-[500px] bg-white p-[50px_30px] flex flex-col gap-5 rounded-3xl border border-[#E5E5E5]">
-                <div className={`flex justify-center text-[#00b13b] text-3xl ${spectral.className}`}>
+                <form action={formAction} className="w-[500px] bg-white p-[50px_30px] flex flex-col gap-5 rounded-3xl border border-[#E5E5E5]">
+                    <div className={`flex justify-center text-[#00b13b] text-3xl ${spectral.className}`}>
                         Shopease
                     </div>
                     <h1 className="font-bold text-2xl text-black leading-[34px]">Sign In</h1>
+
+                    {state.error !== '' && (
+                        <div className="border border-red-300 text-red-500 p-3 rounded">
+                            <h4 className="font-semibold">Error</h4>
+                            <p className="text-sm">{state.error}</p>
+                        </div>
+                    )}
+
                     <div className="flex items-center gap-[10px] rounded-full border border-[#E5E5E5] p-[12px_20px] focus-within:ring-2 focus-within:ring-[#FFC736] transition-all duration-300">
                         <div className="flex shrink-0">
                             <img src="./assets/icons/sms.svg" alt="icon" />
                         </div>
-                        <input type="email" id="" name="" className="appearance-none outline-none w-full placeholder:text-[#616369] placeholder:font-normal font-semibold text-black" placeholder="Write your email address" />
+                        <input type="email" id="email" name="email" className="appearance-none outline-none w-full placeholder:text-[#616369] placeholder:font-normal font-semibold text-black" placeholder="Write your email address" />
                     </div>
                     <div className="flex flex-col gap-[10px]">
                         <div className="flex items-center gap-[10px] rounded-full border border-[#E5E5E5] p-[12px_20px] focus-within:ring-2 focus-within:ring-[#FFC736] transition-all duration-300">
                             <div className="flex shrink-0">
                                 <img src="./assets/icons/lock.svg" alt="icon" />
                             </div>
-                            <input type="password" id="password" name="" className="appearance-none outline-none w-full placeholder:text-[#616369] placeholder:font-normal font-semibold text-black" placeholder="Write your password" />
-                            <button type="button" className="reveal-password flex shrink-0">
+                            <input type="password" id="password" name="password" className="appearance-none outline-none w-full placeholder:text-[#616369] placeholder:font-normal font-semibold text-black" placeholder="Write your password" />
+                            {/* <button type="submit" className="reveal-password flex shrink-0">
                                 <img src="./assets/icons/eye.svg" alt="icon" />
-                            </button>
+                            </button> */}
                         </div>
-                        <a href="" className="text-sm text-[#616369] underline w-fit mr-0 ml-auto">Forgot Password</a>
+                        {/* <a href="" className="text-sm text-[#616369] underline w-fit mr-0 ml-auto">Forgot Password</a> */}
                     </div>
                     <div className="flex flex-col gap-3">
-                        <button type="submit" className="p-[12px_24px] bg-[#0D5CD7] rounded-full text-center font-semibold text-white">Sign In to My Account</button>
+                        <SubmitButton />
                         <a href="signup.html" className="p-[12px_24px] bg-white rounded-full text-center text-black font-semibold border border-[#E5E5E5]">Sign Up</a>
                     </div>
                 </form>
