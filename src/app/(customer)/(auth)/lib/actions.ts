a3e2a0c36bda117022237958999483f5a8cda8a0
sulthan rafi hendriansyah <sulthan19@gmail.com>
2025-01-06 21:29:23 +0700

    feat(auth): integrate customer sign-up functionality

--- src/app/(customer)/(auth)/lib/actions.ts
+++ src/app/(customer)/(auth)/lib/actions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { schemaSignIn } from "@/lib/schema";
+import { schemaSignIn, schemaSignUp } from "@/lib/schema";
 import { ActionResult } from "@/types";
 import prisma from "../../../../../lib/prisma";
 import bcrypt from "bcrypt"
@@ -50,4 +50,41 @@ export async function SignIn(
     cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
 
     return redirect('/')
+}
+
+export async function SignUp(
+    _: unknown,
+    formData: FormData
+): Promise<ActionResult> {
+
+    const parse = schemaSignUp.safeParse({
+        name: formData.get('name'),
+        email: formData.get('email'),
+        password: formData.get('password')
+    })
+
+    if (!parse.success) {
+        return {
+            error: parse.error.errors[0].message
+        }
+    }
+
+    const hashPassword = bcrypt.hashSync(parse.data.password, 12)
+
+    try {
+        await prisma.user.create({
+            data: {
+                email: parse.data.email,
+                name: parse.data.name,
+                password: hashPassword,
+                role: 'customer'
+            }
+        })
+    } catch (error) {
+        return {
+            error: 'Failed to sign up'
+        }
+    }
+
+    return redirect('/sign-in')
 }
